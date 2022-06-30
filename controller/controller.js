ce9a42c2caf3b02af123f4c0739eefd0555a0eaf
Daniel Sarmento <danielsarmento@users.noreply.github.com>
2022-06-30 00:20:11 -0300

    validacaoMatricula

--- controller/controller.js
+++ controller/controller.js
@@ -1,11 +1,16 @@
 const AlunosCadastrados = require("../model/dadosModel");
 
 exports.getAluno = async (req, res) => {
-    try{
-      const Aluno = await AlunosCadastrados.findOne({matricula: req.params.matricula});
-      res.send(Aluno)
-      }
-      catch (err) {
-      res.status(500).send({ error: err.message });
+    const NDG = req.params.matricula;
+    if (NDG === '001' || NDG === '002' || NDG === '003' || NDG === '004' || NDG === '005'){
+        try{
+            const Aluno = await AlunosCadastrados.findOne({matricula: NDG});
+            res.send(Aluno)
+            }
+            catch (err) {
+            res.status(500).send({ error: err.message });
+          }
+    } else {
+        return res.send({erro:'Insira um número de matrícula válido!'})
     }
     };
